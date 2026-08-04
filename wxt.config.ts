f5b122a946f4b37061d7f5baab444d820912d56d
Sigsign <sig@signote.cc>
2026-08-04 12:43:23 +0000

    refactor: move source files to src directory

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
-// See https://wxt.dev/api/config.html
-export default defineConfig({});
+export default defineConfig({
+  srcDir: "src",
+});
