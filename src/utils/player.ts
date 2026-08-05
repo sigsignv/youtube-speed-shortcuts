fd59387cabd151d23b9e133ea9aa0563c3410feb
Sigsign <sig@signote.cc>
2026-08-05 00:30:53 +0000

    feat: add function to find YouTube player

---
+++ src/utils/player.ts
@@ -0,0 +1,33 @@
+export type YouTubePlayerElement = Element & {
+  getAvailablePlaybackRates(): number[];
+  getPlaybackRate(): number;
+  setPlaybackRate(rate: number): void;
+};
+
+const requiredMethods = [
+  "getAvailablePlaybackRates",
+  "getPlaybackRate",
+  "setPlaybackRate",
+] as const;
+
+export function findYouTubePlayer(): YouTubePlayerElement | null {
+  const element = document.querySelector("#movie_player");
+  if (!element || !isYouTubePlayerElement(element)) {
+    return null;
+  }
+
+  return element;
+}
+
+function isYouTubePlayerElement(
+  element: Element,
+): element is YouTubePlayerElement {
+  for (const methodName of requiredMethods) {
+    const method: unknown = Reflect.get(element, methodName);
+    if (typeof method !== "function") {
+      return false;
+    }
+  }
+
+  return true;
+}
